97901528025001921e4ff254eeca9ea8253e8bbb
gachez <briangach98@gmail.com>
2019-12-02 07:41:12 +0300

    adding respective screens

--- src/EventsScreen.js
+++ src/EventsScreen.js
@@ -11,20 +11,7 @@ let styles = StyleSheet.create({
         backgroundColor: 'rgba(255, 255, 255, 0.8)'   
       },
       eventBar: {
-        width: wp('90%'),
-        height: hp('30%'),
-        position: 'relative',
-        top: hp('15%'),
-        left: wp('5%'),
-        borderTopWidth: 2,
-        borderBottomWidth:2,
-        borderLeftWidth: 2,
-        borderRightWidth: 2,
-        borderTopLeftRadius:5,
-        borderBottomLeftRadius: 5,
-        borderBottomRightRadius:5,
-        borderTopRightRadius: 5,
-        borderColor: 'rgba(0,0,0,0.8)' 
+
       }
 
 })
@@ -37,10 +24,87 @@ export default class EventsScreen extends Component{
     render(){
         return(
             <View style={styles.container}>
-
-              <TouchableOpacity style={styles.eventBar}>
-                  
-              </TouchableOpacity>
+            
+            <View style={{flexDirection: 'row', position: 'absolute', top: hp('3.5%'), width: wp('100%')}}>
+            <TouchableOpacity style={{
+                            width: wp('40%'),
+                            height: 59,
+                            left: wp('7.5%'),
+                            borderTopLeftRadius:25,
+                            borderBottomLeftRadius: 25,
+                            borderBottomRightRadius:25,
+                            borderTopRightRadius: 25,
+                            backgroundColor: 'rgba(rgba(255, 195, 0,0.9))'                 
+            }}><Text style={{fontSize: 25, color: 'rgba(255, 255, 255,0.9)', paddingTop: 10, paddingLeft: 25}}>Upcoming</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{
+                            width: wp('40%'),
+                            height: 59,
+                            left: wp('11.5%'),
+                            borderColor: 'rgba(0,0,0,0.2)',
+                            borderTopWidth: 1,
+                            borderBottomWidth:1,
+                            borderLeftWidth: 1,
+                            borderRightWidth:1,
+                            borderTopLeftRadius:25,
+                            borderBottomLeftRadius: 25,
+                            borderBottomRightRadius:25,
+                            borderTopRightRadius: 25                 
+            }}><Text style={{fontSize: 25, paddingTop: 10, paddingLeft: 25, color: 'rgba(0,0,0,0.5)'}}>Ongoing</Text></TouchableOpacity>
+            </View>
+            
+            
+            <View style={{flex: 1}}>
+            <ScrollView style={{ top: hp('15%'), width: wp('100%')}}>
+                <View  style={{
+                            width: wp('90%'),
+                            height: 250,
+                            left: wp('5%'),
+                            borderTopWidth: 1,
+                            borderBottomWidth:1,
+                            borderLeftWidth: 1,
+                            borderRightWidth: 1,
+                            borderTopLeftRadius:25,
+                            borderBottomLeftRadius: 25,
+                            borderBottomRightRadius:25,
+                            borderTopRightRadius: 25,
+                            borderColor: 'rgba(0,0,0,0.5)' 
+                }}></View>
+                        <View  style={{
+                            top: 15,
+                            paddingTop: 10,
+                            width: wp('90%'),
+                            height: 250,
+                            left: wp('5%'),
+                            borderTopWidth: 1,
+                            borderBottomWidth:1,
+                            borderLeftWidth: 1,
+                            borderRightWidth: 1,
+                            borderTopLeftRadius:25,
+                            borderBottomLeftRadius: 25,
+                            borderBottomRightRadius:25,
+                            borderTopRightRadius: 25,
+                            borderColor: 'rgba(0,0,0,0.5)' 
+                }}></View>
+                   <View  style={{
+                            top: 30,
+                            width: wp('90%'),
+                            height: 250,
+                            left: wp('5%'),
+                            borderTopWidth: 1,
+                            borderBottomWidth:1,
+                            borderLeftWidth: 1,
+                            borderRightWidth: 1,
+                            borderTopLeftRadius:25,
+                            borderBottomLeftRadius: 25,
+                            borderBottomRightRadius:25,
+                            borderTopRightRadius: 25,
+                            borderColor: 'rgba(0,0,0,0.5)' 
+                }}></View>
+            </ScrollView>
+            </View>
+            
+              
                             
             </View>
         )
